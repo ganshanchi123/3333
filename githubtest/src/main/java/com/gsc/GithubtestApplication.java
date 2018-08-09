6164fce6702ae9cb39352403d485d3061d407a29
gsc_glb <2931320314@qq.com>
2018-08-09 09:42:47 +0800

    123

---
+++ githubtest/src/main/java/com/gsc/GithubtestApplication.java
@@ -0,0 +1,12 @@
+package com.gsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubtestApplication.class, args);
+	}
+}
